2124ed30ecfe6773116334f8c4c09e9e16e309fb
Vlad Zhavoronkov <vlad.zhavoronkov@bytepace.com>
2019-07-22 14:58:33 +0600

    Initial Commit

---
+++ AssessmentDatabase/AssessmentDatabase.h
@@ -0,0 +1,19 @@
+//
+//  AssessmentDatabase.h
+//  AssessmentDatabase
+//
+//  Created by Vlad Zhavoronkov  on 7/22/19.
+//  Copyright © 2019 Bytepace. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for AssessmentDatabase.
+FOUNDATION_EXPORT double AssessmentDatabaseVersionNumber;
+
+//! Project version string for AssessmentDatabase.
+FOUNDATION_EXPORT const unsigned char AssessmentDatabaseVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AssessmentDatabase/PublicHeader.h>
+
+
