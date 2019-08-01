4d9b5a278e8f9b70a7a1ecb6c5e753638d6a9c93
Vlad Zhavoronkov <vlad.zhavoronkov@bytepace.com>
2019-08-01 17:19:04 +0600

    Add `EntityUtilsError`

---
+++ AssessmentDatabase/Helpers/EntityUtilsError.swift
@@ -0,0 +1,13 @@
+//
+//  EntityUtilsError.swift
+//  AssessmentDatabase
+//
+//  Created by Vlad Zhavoronkov  on 8/1/19.
+//  Copyright © 2019 Bytepace. All rights reserved.
+//
+
+import Foundation
+
+public enum EntityUtilsError: Error {
+    case entityNotFound
+}
---
+++ AssessmentDatabase/ValueObjects/EntityFields.swift
@@ -0,0 +1,9 @@
+//
+//  EntityFields.swift
+//  AssessmentDatabase
+//
+//  Created by Vlad Zhavoronkov  on 8/1/19.
+//  Copyright © 2019 Bytepace. All rights reserved.
+//
+
+import Foundation
