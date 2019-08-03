5b0f419a2b44a901aa4736998498a89625d38063
Vlad Zhavoronkov <vlad.zhavoronkov@bytepace.com>
2019-08-03 18:15:27 +0600

    Add relations functionality to Students Implement set methods for `StudentsUtils`. Add test for this new features. Because of changes in `StudentsUtils` several tests are failed now

--- AssessmentDatabase/Utils/StudentsUtils.swift
+++ AssessmentDatabase/Utils/StudentsUtils.swift
@@ -8,6 +8,10 @@ public class StudentsUtils: EntityUtils {
     public var container: NSPersistentContainer
     public lazy var backgroundContext = container.newBackgroundContext()
     
+    public var assessmentsUtils: AssessmentsUtils?
+    public var instructorsUtils: InstructorsUtils?
+    public var microtaskGradesUtils: StudentMicrotaskGradesUtils?
+    
     public init(with container: NSPersistentContainer) {
         self.container = container
     }
@@ -17,14 +21,80 @@ public class StudentsUtils: EntityUtils {
     }
     
     public func setRelations(of entity: Student, like item: StudentFields) throws {
-        #warning("Nothing is implemented")
+        do {
+            try set(assessments: item.assessments, of: entity)
+            try set(instructors: item.instructors, of: entity)
+            try set(microtaskGrades: item.microTaskGrades, of: entity)
+        } catch {
+            throw error
+        }
     }
     
-    public func update(item: StudentFields) {
-        fatalError()
+    private func set(assessments: [AssessmentFields], of student: Student) throws {
+        guard !assessments.isEmpty else {
+            return
+        }
+        guard let util = assessmentsUtils else {
+            throw Errors.noUtils
+        }
+        let savedAssessments = util.get(whereSids: assessments.map { $0.sid })
+        guard !savedAssessments.isEmpty,
+            let backgroundContextAssessments = savedAssessments
+                .map({ backgroundContext.object(with: $0.objectID) }) as? [Assessment]
+        else {
+            throw Errors.assessmentsNotFound
+        }
+        student.addToAssessments(NSSet(array: backgroundContextAssessments))
+        backgroundContextAssessments.forEach {
+            $0.addToStudents(student)
+        }
     }
     
-    public func update(items: [StudentFields]) {
-        fatalError()
+    private func set(instructors: [InstructorFields], of student: Student) throws {
+        guard !instructors.isEmpty else {
+            return
+        }
+        guard let util = instructorsUtils else {
+            throw Errors.noUtils
+        }
+        let savedInstructors = util.get(whereSids: instructors.map { $0.sid })
+        guard !savedInstructors.isEmpty,
+            let backgroundContextInstructors = savedInstructors
+                .map({ backgroundContext.object(with: $0.objectID) }) as? [Instructor]
+        else {
+            throw Errors.instructorsNotFound
+        }
+        student.addToInstructors(NSSet(array: backgroundContextInstructors))
+        backgroundContextInstructors.forEach {
+            $0.addToStudents(student)
+        }
+    }
+    
+    private func set(microtaskGrades: [StudentMicrotaskGradeFields], of student: Student) throws {
+        guard !microtaskGrades.isEmpty else {
+            return
+        }
+        guard let util = microtaskGradesUtils else {
+            throw Errors.noUtils
+        }
+        let savedMicrotaskGrades = util.get(whereSids: microtaskGrades.map { $0.sid })
+        guard !savedMicrotaskGrades.isEmpty,
+            let backgroundContextMicrotaskGrades = savedMicrotaskGrades
+                .map({ backgroundContext.object(with: $0.objectID) }) as? [StudentMicrotaskGrade]
+        else {
+            throw Errors.microtaskGradesNotFound
+        }
+        student.addToMicroTaskGrades(NSSet(array: backgroundContextMicrotaskGrades))
+        backgroundContextMicrotaskGrades.forEach {
+            $0.student = student
+        }
+    }
+    
+    public enum Errors: Error {
+        case noUtils
+        
+        case assessmentsNotFound
+        case instructorsNotFound
+        case microtaskGradesNotFound
     }
 }
--- AssessmentDatabaseTests/StudentsUtilsTest.swift
+++ AssessmentDatabaseTests/StudentsUtilsTest.swift
@@ -10,25 +10,93 @@ import XCTest
 
 class StudentsUtilsTest: XCTestCase {
     typealias This = StudentsUtilsTest
-    static let util = StudentsUtils(with: getMockPersistentContainer())
+    static let container = getMockPersistentContainer()
+    static let util: StudentsUtils = {
+        let util = StudentsUtils(with: container)
+        util.assessmentsUtils = This.assessmentsUtils
+        util.instructorsUtils = This.instructorsUtils
+        util.microtaskGradesUtils = This.microtaskGradesUtils
+        return util
+    }()
+    static let rubricsUtils = RubricsUtils(with: container)
+    static let skillSetsUtils: SkillSetsUtils = {
+        let util = SkillSetsUtils(with: container)
+        util.rubricUtils = This.rubricsUtils
+        return util
+    }()
+    static let microtasksUtils: MicrotasksUtils = {
+        let util = MicrotasksUtils(with: container)
+        util.skillSetsUtils = This.skillSetsUtils
+        return util
+    }()
+    static let gradesUtils = GradesUtils(with: container)
+    static let microtaskGradesUtils: StudentMicrotaskGradesUtils = {
+        let util = StudentMicrotaskGradesUtils(with: container)
+        util.microtasksUtils = This.microtasksUtils
+        util.assessmentsUtils = This.assessmentsUtils
+        util.gradesUtils = This.gradesUtils
+        return util
+    }()
+    static let instructorsUtils = InstructorsUtils(with: container)
+    static let assessmentsUtils = AssessmentsUtils(container: container)
     static let context = util.container.viewContext
+    
     private let mockStudents = Mocks.mockStudents
+    private let mockRubrics = Mocks.mockEmptyRubrics
+    private let mockInstructors = Mocks.mockEmptyInstructors
+    private let mockAssessments = Mocks.mockEmptyAssessments
+    private let mockGrades = Mocks.mockGrades
+    private let mockSkillSets = Mocks.mockSkillSets.reduce([]) { $0 + $1 }
+    private let mockMicroTasks = Mocks.mockMicrotasks.reduce([]) { $0 + $1 }
+    private let microtasksGrades = Mocks.mockMicrotaskGrades
+    
+    override func setUp() {
+        super.setUp()
+        
+        This.microtaskGradesUtils.studentsUtils = This.util
+        
+        This.assessmentsUtils.rubricsUtils = This.rubricsUtils
+        This.assessmentsUtils.instructorsUtils = This.instructorsUtils
+        
+        XCTAssertNoThrow(try This.gradesUtils.save(items: mockGrades))
+        XCTAssertNoThrow(try This.rubricsUtils.save(items: mockRubrics))
+        XCTAssertNoThrow(try This.skillSetsUtils.save(items: mockSkillSets))
+        XCTAssertNoThrow(try This.microtasksUtils.save(items: mockMicroTasks))
+        XCTAssertNoThrow(try This.instructorsUtils.save(items: mockInstructors))
+        XCTAssertNoThrow(try This.assessmentsUtils.save(items: mockAssessments))
+    }
     
     override func tearDown() {
         super.tearDown()
+        This.rubricsUtils.deleteAll()
+        This.instructorsUtils.deleteAll()
+        This.assessmentsUtils.deleteAll()
         This.util.deleteAll()
+        
+        XCTAssertTrue(This.rubricsUtils.getAll().isEmpty)
+        XCTAssertTrue(This.instructorsUtils.getAll().isEmpty)
+        XCTAssertTrue(This.assessmentsUtils.getAll().isEmpty)
         XCTAssertTrue(This.util.getAll().isEmpty)
     }
     
     func testSaveItem() {
-        let item = mockStudents[0]
-        XCTAssertNoThrow(try This.util.save(item: item))
+        var item = mockStudents[0]
+        item.microTaskGrades = microtasksGrades.filter { $0.student.sid == item.sid }
+        var clearedStudent = item
+        clearedStudent.microTaskGrades = []
+        XCTAssertNoThrow(try This.util.save(item: clearedStudent))
+        XCTAssertNoThrow(try This.microtaskGradesUtils.save(items: item.microTaskGrades))
         compareItems([item], This.util.getAll())
     }
     
     func testSaveItems() {
-        let items = Array(mockStudents[1..<mockStudents.count])
-        XCTAssertNoThrow(try This.util.save(items: items))
+        let items = Array(mockStudents)
+        var clearedItems = items
+        for index in clearedItems.indices {
+            clearedItems[index].microTaskGrades = []
+        }
+        XCTAssertNoThrow(try This.util.save(items: clearedItems))
+        XCTAssertNoThrow(try This.microtaskGradesUtils.save(items: items.map({ $0.microTaskGrades }).reduce([]) { $0 + $1 }))
         compareItems(items, This.util.getAll())
     }
     
@@ -45,6 +113,8 @@ class StudentsUtilsTest: XCTestCase {
     }
     
     private func compareItem(_ item: MockStudentFields, _ entity: Student) {
-        
+        XCTAssertEqual(item.assessments.count, entity.assessments?.count)
+        XCTAssertEqual(item.instructors.count, entity.instructors?.count)
+        XCTAssertEqual(item.microTaskGrades.count, entity.microTaskGrades?.count)
     }
 }
